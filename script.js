d3836cbb26b87f183196d922287a943b944059ac
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-07-29 17:40:02 +0100

    :construction: Define the constant for some DOM element

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+//DOM
+const headerEmoji = document.getElementById("headerEmoji");
+const points = document.getElementById("points");
+const hiScore = document.getElementById("hiScore");
+const display = document.querySelector(".display");
+const info = document.querySelector(".info");
