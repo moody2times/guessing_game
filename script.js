ce3b2fe43464bec5ba8619fa2e005c8d2417c8e2
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-08-02 11:02:05 +0100

    :lipstick: Update the logic when player number is NaN

--- script.js
+++ script.js
@@ -30,18 +30,19 @@ console.log(secretNumber);
 
 //When user enter a number and press check
 const onCheck = () => {
-	//What happens if player press check but entered no number
-	if (!playerNumber.value) {
-		info.textContent = `Choose a number from 1 - 20 and try again!!`;
+	//convert user number to a Number and reset the form
+	const convertedPlayerNumber = parseInt(playerNumber.value);
+	form.reset();
+
+	//What happens if player press check but entered no number or player's number is NaN
+	console.log(convertedPlayerNumber);
+	if (!convertedPlayerNumber) {
+		info.textContent = `Not a number!!! Please try again`;
 		setTimeout(() => {
 			info.textContent = "\xa0";
 		}, 2000);
 		return;
 	}
-
-	//convert user number to a Number and reset the form
-	const convertedPlayerNumber = parseInt(playerNumber.value);
-	form.reset();
 
 	//Validation for user entry
 	if (convertedPlayerNumber < 1 || convertedPlayerNumber > 20) {
