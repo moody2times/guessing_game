deb7ae7e4e5e1faabfa67644d80209ae0a6de47a
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-08-25 20:00:36 +0100

    :fire: Delete completed todo

--- script.js
+++ script.js
@@ -220,7 +220,6 @@ const onSubmit = (event) => {
 		}
 	}
 
-	//BUG: When player use hint and points go below zero, the game does not end
 	//what happens when player press use hint buttons
 	if (event.submitter.id === "hintOne") {
 		//what happens if player use hint one
